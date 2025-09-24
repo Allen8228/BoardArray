d15d50a84db7fe7bd008e7ba21e32ace48945556
Rik allen Tadic <rikallen.tadic@cit.edu>
2025-09-25 03:00:09 +0800

    feat: implement add() method for boardarray

---
+++ src/tests/boardarray.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "board.h"
+#define SIZE 5
+using namespace std;
+
+class BoardArray : public Board {
+    Entry* array;
+    int index;
+
+    public:
+        BoardArray() {
+            array = new Entry[SIZE];
+            index = 0;
+        }
+
+        void add(Entry* entry) {
+            // TODO: IMPLEMENT THIS FUNCTION
+            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+             if (index == 0) {
+                array[0] = *entry;
+                index++;
+                return;
+            }
+             int pos = index; // default = last position
+            for (int i = 0; i < index; i++) {
+                if (entry->compare(&array[i])) { // if new score > current score
+                    pos = i;
+                    break;
+                     }
+            }
+             if (index == SIZE && pos == index) {
+                cout << entry->name << "'s score is too low to be added!" << endl;
+                return;
+             }
+              if (index < SIZE) {
+                index++; // we have room → increase count
+            }
+            for (int j = index - 1; j > pos; j--) {
+                array[j] = array[j - 1];
+            }
+              array[pos] = *entry;
+        
+
+        }
+
+        void print() {
+            for (int i = 0; i < index; i++) {
+                cout << i + 1 << ". ";
+                array[i].print();
+            }
+        }
+};
